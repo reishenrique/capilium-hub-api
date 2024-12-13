6ac8d90a00608e1298793782c0dfd82840030bb5
Henrique Reis <contatohenriquereis@gmail.com>
2024-12-13 15:12:38 -0300

    refactor: adding logger at application startup, cors configuration and bodyParser

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, type NestApplicationOptions } from '@nestjs/common';
+import * as bodyParser from 'body-parser'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const configNest: NestApplicationOptions = {
+    cors: true
+  }
+  
+  const app = await NestFactory.create(AppModule, configNest);
+
+  app.use(bodyParser.json({ limit: '150mb'}))
+
+  await app.listen(process.env.PORT || 3000, () => {
+    Logger.log(`Server listen on port: ${process.env.PORT || 3000}`, 'InitServer')
+  });
 }
 bootstrap();
