b9755ca2a17396bc23b29b9edcb4dab84907ec70
Henrique Reis <contatohenriquereis@gmail.com>
2024-12-14 21:24:47 -0300

    feat: creation of user schema

---
+++ src/users/schemas/users.schema.ts
@@ -0,0 +1,9 @@
+import { SchemaFactory } from "@nestjs/mongoose";
+import { UserEntity } from "../entity/users.entity";
+import type { Document } from "mongoose"
+
+export type UserDocument = UserEntity & Document
+
+export const UsersSchema = SchemaFactory.createForClass(
+    UserEntity
+)
