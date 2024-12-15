ec07a3d6db74a42088bef64fe225710dfe7dc231
Henrique Reis <contatohenriquereis@gmail.com>
2024-12-14 21:22:58 -0300

    config: integrating mongoose into the project, and defining protection throttler for api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,23 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(process.env.DATABASE_URL, {
+      maxPoolSize: Number(process.env.MAX_POOL_SIZE_MONGO) || 50,
+      minPoolSize: Number(process.env.MIN_POOL_SIZE_MONGO) || 5
+    }),
+    ThrottlerModule.forRoot([
+      {
+        ttl: Number(process.env.RATE_LIMIT_TIME) || 10000,
+        limit: Number(process.env.RATE_LIMIT_AMOUNT_REQUESTS) || 10,
+        skipIf(context) {
+          return context.getClass().name !== 'UserController'
+        }
+      }
+    ])
+  ],
   controllers: [],
   providers: [],
 })
