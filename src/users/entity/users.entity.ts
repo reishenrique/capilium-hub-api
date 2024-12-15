69ab600bd884888a68203513362549df3df148d4
Henrique Reis <contatohenriquereis@gmail.com>
2024-12-15 14:26:27 -0300

    feat: creating platform user entity

---
+++ src/users/entity/users.entity.ts
@@ -0,0 +1,40 @@
+import { Prop, Schema } from "@nestjs/mongoose";
+import { ApiProperty } from "@nestjs/swagger";
+import type { AvailabilityStatusEnum } from "../enum/availability.enum";
+import type { ProfessionEnum } from "../enum/profession.enum";
+import type { SpecializationEnum } from "../enum/specialization.enum";
+
+@Schema({ timestamps: true })
+export class UserEntity {
+    @ApiProperty()
+    @Prop()
+    firstName: string
+    
+    @ApiProperty()
+    @Prop()
+    lastName: string
+
+    @ApiProperty()
+    @Prop()
+    cpf: string
+
+    @ApiProperty()
+    @Prop()
+    profession: ProfessionEnum
+
+    @ApiProperty()
+    @Prop()
+    specializaion: SpecializationEnum[]
+
+    @ApiProperty()
+    @Prop()
+    availabilityStatus: AvailabilityStatusEnum
+
+    @ApiProperty()
+    @Prop()
+    professionalExperience: string
+
+    @ApiProperty({ required: false })
+    @Prop()
+    portfolio: string
+}
