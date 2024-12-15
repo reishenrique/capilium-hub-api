0fbbb70dd5ee3ce96eef12c741252913e1786a56
Henrique Reis <contatohenriquereis@gmail.com>
2024-12-14 21:23:24 -0300

    feat: creating user module

---
+++ src/users/user.module.ts
@@ -0,0 +1,28 @@
+import { Module } from "@nestjs/common";
+import { MongooseModule } from '@nestjs/mongoose'
+import { UserController } from "./controller/user.controller";
+import { UserEntity } from "./entity/users.entity";
+import { UsersSchema } from "./schemas/users.schema";
+import { UserService } from "./service/user.service";
+import { UserRepository } from "./repository/user.repository"
+import userProvider from "./user.provider";
+
+@Module({
+    controllers: [UserController],
+    imports: [
+        MongooseModule.forFeature([
+            {
+                name: UserEntity.name,
+                schema: UsersSchema
+            },
+        ]),
+    ],
+    providers: userProvider,
+    exports: [
+        MongooseModule,
+        UserRepository,
+        UserService
+    ]
+})
+
+export class UserModule {}
