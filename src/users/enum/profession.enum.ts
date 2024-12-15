671c14c274bba0652ce7c30ccf26569647c27069
Henrique Reis <contatohenriquereis@gmail.com>
2024-12-15 14:25:36 -0300

    feat: availability, profession and specialization enums

---
+++ src/users/enum/profession.enum.ts
@@ -0,0 +1,7 @@
+export enum ProfessionEnum {
+  Dermatologist = "Dermatologist",
+  PlasticSurgeon = "Plastic Surgeon",
+  Nurse = "Nurse",
+  Trichologist = "Trichologist",
+  Anesthesiologist = "Anesthesiologist",
+}
---
+++ src/users/enum/specialization.enum.ts
@@ -0,0 +1,7 @@
+export enum SpecializationEnum {
+  HairTransplant = "Hair Transplant",
+  ScalpTreatment = "Scalp Treatment",
+  LocalAnesthesia = "Local Anesthesia",
+  AestheticProcedures = "Aesthetic Procedures",
+  PsychologicalSupport = "Psychological Support",
+}
