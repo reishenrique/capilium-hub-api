73e9df3ec0804ec587c01fbe1aa2f94b0f52f632
Henrique Reis <contatohenriquereis@gmail.com>
2024-12-16 14:40:39 -0300

    feat: implementing the user service with dependency user repository

--- src/users/service/user.service.ts
+++ src/users/service/user.service.ts
@@ -1,1 +1,15 @@
-export class UserService {}
+import { Injectable, Logger } from '@nestjs/common';
+import type { UserRepository } from '../repository/user.repository';
+import type { UserEntity } from '../entity/users.entity';
+import type { UserDTO } from '../dto/user.dto';
+
+@Injectable()
+export class UserService {
+	protected readonly _logger = new Logger('UserService');
+	constructor(private readonly userRepository: UserRepository) {}
+
+	async newUser(user: UserDTO): Promise<UserEntity> {
+		const newUser = await this.userRepository.createUser(user);
+		return newUser;
+	}
+}
