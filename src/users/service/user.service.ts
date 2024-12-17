8f724772d4ad750c59af9e461167e5f98c3cb5aa
Henrique Reis <contatohenriquereis@gmail.com>
2024-12-17 19:24:35 -0300

    feat: adding some verifications for duplicates and implementing get user by id service

--- src/users/service/user.service.ts
+++ src/users/service/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { ConflictException, Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { UserRepository } from '../repository/user.repository';
 import { CreateUserDto } from '../dto/createUserDto';
 import { ResponseUserDto } from '../dto/responseUserDto';
@@ -11,7 +11,26 @@ export class UserService {
 	async newUser(
 		user: CreateUserDto,
 	): Promise<Partial<ResponseUserDto>> {
+		const { cpf }: { cpf: string } = user
+	
+		const userExistsByCpf = await this.userRepository.getUserByCpf(cpf)
+
+		if (userExistsByCpf) {
+			throw new ConflictException('CPF already registered in the system')
+		}
+
 		const newUser = await this.userRepository.createUser(user);
+
 		return newUser;
+	}
+
+	async getUserById(id: string): Promise<Partial<ResponseUserDto>> {
+		const user = await this.userRepository.getUserById(id)
+
+		if (!user) {	
+			throw new NotFoundException('User not found')
+		}
+
+		return user
 	}
 }
