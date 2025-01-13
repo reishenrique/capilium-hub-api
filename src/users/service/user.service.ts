1f8dc74a11f59ed75fb176d1aa9e164be630707e
Henrique Reis <contatohenriquereis@gmail.com>
2025-01-13 19:00:38 -0300

    refactor: implementing dependency conversion in user service

--- src/users/service/user.service.ts
+++ src/users/service/user.service.ts
@@ -4,29 +4,32 @@ import {
 	Logger,
 	NotFoundException,
 } from '@nestjs/common';
-import { UserRepository } from '../repository/user.repository';
 import { CreateUserDto } from '../dto/createUserDto';
 import { ResponseUserDto } from '../dto/responseUserDto';
 import { removeNonNumeric } from 'src/helpers/cleaners';
 import bcrypt from 'bcrypt';
+import { IUserRepository } from '../interfaces/IUserRepository';
 
 @Injectable()
 export class UserService {
 	protected readonly _logger = new Logger('UserService');
-	constructor(private readonly userRepository: UserRepository) {}
+	constructor(private readonly userRepository: IUserRepository) {}
 
 	async newUser(userPayload: CreateUserDto): Promise<Partial<ResponseUserDto>> {
 		const { cpf, email }: { cpf: string; email: string } = userPayload;
 
 		const formattedCpf = removeNonNumeric(cpf);
 
-		const userExistsByCpf = await this.userRepository.getUserByCpf(formattedCpf);
+		const userExistsByCpf =
+			await this.userRepository.getUserByCpf(formattedCpf);
 
-		if (userExistsByCpf) throw new ConflictException('CPF already registered in the system');
-		
-		const userExistsByEmail = await this.userRepository.getUserByEmail(email)
+		if (userExistsByCpf)
+			throw new ConflictException('CPF already registered in the system');
 
-		if (userExistsByEmail) throw new ConflictException('Email already registered in the system')
+		const userExistsByEmail = await this.userRepository.getUserByEmail(email);
+
+		if (userExistsByEmail)
+			throw new ConflictException('Email already registered in the system');
 
 		const saltRounds = await bcrypt.genSalt(10);
 		const hashPassword = await bcrypt.hash(userPayload.password, saltRounds);
