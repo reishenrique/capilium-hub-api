086618532a286c219913f417933bc57fc88d9ddb
Henrique Reis <contatohenriquereis@gmail.com>
2025-01-17 18:51:26 -0300

    refactor: optional ttl

--- src/infrastructure/cache/cacheStrategy/abstractCacheStrategy.ts
+++ src/infrastructure/cache/cacheStrategy/abstractCacheStrategy.ts
@@ -1,7 +1,7 @@
 import { ICache } from '../interfaces/ICache';
 
 export default abstract class CacheStrategy implements ICache {
-	abstract setOnCache(key: string, value: any, ttl: number): boolean;
+	abstract setOnCache(key: string, value: any, ttl?: number): boolean;
 	abstract getOnCache(key: string): any;
 	abstract deleteFromCache(key: string): void;
 	abstract clearCache(): void;
