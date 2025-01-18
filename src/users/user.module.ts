fd16d80b02e0ce30c488223b4b45ee5035a2ba46
Henrique Reis <contatohenriquereis@gmail.com>
2025-01-18 20:48:24 -0300

    feat: add cache module in provider user module

--- src/users/user.module.ts
+++ src/users/user.module.ts
@@ -5,9 +5,11 @@ import { UserEntity } from './entity/users.entity';
 import { UsersSchema } from './schemas/users.schema';
 import { UserService } from './service/user.service';
 import { UserRepository } from './repository/user.repository';
+import { CacheModule } from 'src/infrastructure/cache/cache.module';
 
 @Module({
 	imports: [
+		CacheModule,
 		MongooseModule.forFeature([
 			{
 				name: UserEntity.name,
