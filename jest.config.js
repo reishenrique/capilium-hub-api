8a2345683ad2f81c1ea1f15ff5d1c4901bcc5528
Henrique Reis <contatohenriquereis@gmail.com>
2025-01-20 21:28:28 -0300

    chore: setting jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-	preset: 'ts-jest',
-	testEnvironment: 'node',
-	testMatch: ['**/src/**/*.spec.ts'],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/__test__/**/*.spec.ts'],
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1',
+  },
 };
