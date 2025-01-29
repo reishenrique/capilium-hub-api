a6cf23cbef7389933b769af4bfdd1a1138444456
Henrique Reis <contatohenriquereis@gmail.com>
2025-01-29 15:02:04 -0300

    refactor: adjusting return using the clinic response dto

--- src/clinic/presentation/clinic.controller.ts
+++ src/clinic/presentation/clinic.controller.ts
@@ -11,35 +11,38 @@ import {
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { ClinicService } from '../clinic.service';
 import { CreateClinicDto } from '../dto/createClinicDto';
+import { ClinicResponseDtoSwagger } from '../dto/clinicResponseDto';
 
 @ApiTags('clinic')
 @Controller('clinic')
 export class ClinicController {
-		protected readonly _logger = new Logger(ClinicController.name);
-		constructor(private readonly clinicService: ClinicService) {}
+	protected readonly _logger = new Logger(ClinicController.name);
+	constructor(private readonly clinicService: ClinicService) {}
 
-		@Post('/')
-		@HttpCode(HttpStatus.CREATED)
-		@ApiOperation({ summary: 'Create a new clinic' })
-		@ApiResponse({ status: 201 })
-		@ApiResponse({
-			status: 409,
-			description: 'CNPJ already registered in the system',
-		})
-		@ApiResponse({
-			status: 400,
-			description: 'Error when trying to created a new clinic',
-		})
-		public async create(@Body() clinic: CreateClinicDto): Promise<object> {
-			try {
-				const newClinic = await this.clinicService.create(clinic);
+	@Post('/')
+	@HttpCode(HttpStatus.CREATED)
+	@ApiOperation({ summary: 'Create a new clinic' })
+	@ApiResponse({ status: 201, type: ClinicResponseDtoSwagger })
+	@ApiResponse({
+		status: 409,
+		description: 'CNPJ already registered in the system',
+	})
+	@ApiResponse({
+		status: 400,
+		description: 'Error when trying to created a new clinic',
+	})
+	public async create(
+		@Body() clinic: CreateClinicDto,
+	): Promise<Partial<ClinicResponseDtoSwagger>> {
+		try {
+			const newClinic = await this.clinicService.create(clinic);
 
-				return newClinic;
-			} catch (error) {
-				if (error instanceof ConflictException) throw error;
+			return newClinic;
+		} catch (error) {
+			if (error instanceof ConflictException) throw error;
 
-				this._logger.error('Error when trying to created a new clinic');
-				throw new InternalServerErrorException(error.message);
-			}
+			this._logger.error('Error when trying to created a new clinic');
+			throw new InternalServerErrorException(error.message);
 		}
 	}
+}
