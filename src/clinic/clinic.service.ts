6750d48089c3856b68acc91bf6dd5f539d9fbe42
Henrique Reis <contatohenriquereis@gmail.com>
2025-01-30 18:12:05 -0300

    feat: implementing find all methods to list clinics

--- src/clinic/clinic.service.ts
+++ src/clinic/clinic.service.ts
@@ -1,14 +1,21 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+	ConflictException,
+	Injectable,
+	NotFoundException,
+} from '@nestjs/common';
 import { ClinicRepository } from './repository/clinic.repository';
 import { Logger } from '@nestjs/common';
 import { CreateClinicDto } from './dto/createClinicDto';
+import { ClinicResponseDtoSwagger } from './dto/clinicResponseDto';
 
 @Injectable()
 export class ClinicService {
 	protected readonly _logger = new Logger('ClinicService');
 	constructor(private readonly clinicRepository: ClinicRepository) {}
 
-	async create(clinicPayload: CreateClinicDto): Promise<object> {
+	async create(
+		clinicPayload: CreateClinicDto,
+	): Promise<Partial<ClinicResponseDtoSwagger>> {
 		const findClinicByCnpj = await this.clinicRepository.findClinicByCnpj(
 			clinicPayload.cnpj,
 		);
@@ -20,5 +27,15 @@ export class ClinicService {
 		const newClinic = await this.clinicRepository.createClinic(clinicPayload);
 
 		return newClinic;
+	}
+
+	async findAll(): Promise<ClinicResponseDtoSwagger[]> {
+		const findAllClinics = await this.clinicRepository.findAllClinics();
+
+		if (!findAllClinics.length) {
+			throw new NotFoundException('No clinics found');
+		}
+
+		return findAllClinics;
 	}
 }
