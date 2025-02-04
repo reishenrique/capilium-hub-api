409018dd6f97c8f73248d15718fe2a28fcad24a0
Henrique Reis <contatohenriquereis@gmail.com>
2025-02-04 17:42:40 -0300

    feat: setting env queue port to queue application module

--- apps/queue/src/main.ts
+++ apps/queue/src/main.ts
@@ -3,6 +3,6 @@ import { QueueModule } from './queue.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(QueueModule);
-  await app.listen(process.env.port ?? 3000);
+  await app.listen(process.env.QUEUE_PORT);
 }
 bootstrap();
