2363c081da59c0d292a563d1434b340689bc3ad1
Henrique Reis <contatohenriquereis@gmail.com>
2025-02-05 18:45:21 -0300

    fix: adjusting smtp environment variables

--- apps/capilium-hub-api/src/common/utils/mailerUtils.ts
+++ apps/capilium-hub-api/src/common/utils/mailerUtils.ts
@@ -2,7 +2,7 @@ import nodemailer from 'nodemailer';
 import SMTPTransport from 'nodemailer/lib/smtp-transport';
 
 const transporter = nodemailer.createTransport({
-	host: process.env.MAIL_USER,
+	host: process.env.MAIL_HOST,
 	port: process.env.MAIL_PORT,
 	auth: {
 		user: process.env.MAIL_USER,
