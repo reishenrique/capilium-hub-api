909d49b73f8cbf1ad6afe8940b9190c353d58566
Henrique Reis <contatohenriquereis@gmail.com>
2025-02-05 18:48:38 -0300

    feat: registering the function that processes the email that is queued in the queue module

--- apps/queue/src/queue.module.ts
+++ apps/queue/src/queue.module.ts
@@ -1,6 +1,7 @@
 import { EMAIL_QUEUE, SharedModule } from '@app/shared';
 import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
+import { EmailProcessor } from './mailer/mailer.processor';
 
 @Module({
 	imports: [
@@ -9,5 +10,6 @@ import { Module } from '@nestjs/common';
 			name: EMAIL_QUEUE,
 		}),
 	],
+	providers: [EmailProcessor],
 })
 export class QueueModule {}
