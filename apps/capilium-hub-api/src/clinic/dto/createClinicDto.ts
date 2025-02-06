ddfa6eb482408e840142faba21615ebcc40fa986
Henrique Reis <contatohenriquereis@gmail.com>
2025-02-06 19:39:53 -0300

    fea: adding swagger documentation in create clinic dto

--- apps/capilium-hub-api/src/clinic/dto/createClinicDto.ts
+++ apps/capilium-hub-api/src/clinic/dto/createClinicDto.ts
@@ -10,53 +10,80 @@ import { SocialNetworksEnum } from '../../common/enums/social-networks.enum';
 import { SpecializationEnum } from '../../common/enums/specialization.enum';
 
 export class CreateClinicDto {
-	@ApiProperty()
-	@IsNotEmpty()
-	@IsString()
+	@ApiProperty({
+		example: 'John Hair Clinic',
+		description: 'Name of the clinic you are registering with',
+	})
+	@IsNotEmpty({ message: 'The "clinicName" is required' })
+	@IsString({ message: 'The "clinicName" field must be a string' })
 	clinicName: string;
 
-	@ApiProperty()
-	@IsNotEmpty()
-	@IsString()
+	@ApiProperty({
+		example: '350 5th Ave, New York',
+		description: 'Clinic address',
+	})
+	@IsNotEmpty({ message: 'The "address" is required' })
+	@IsString({ message: 'The "address" field must be a string' })
 	address: string;
 
-	@ApiProperty()
-	@IsNotEmpty()
-	@IsString()
+	@ApiProperty({ example: '12345678', description: 'Clinic contact' })
+	@IsNotEmpty({ message: 'The "address" is required' })
+	@IsString({ message: 'The "address" field must be a string' })
 	contact: string;
 
-	@ApiProperty()
-	@IsNotEmpty()
-	@IsEnum(SocialNetworksEnum)
+	@ApiProperty({ example: 'Instagram', description: 'Clinic social network' })
+	@IsNotEmpty({ message: 'The "socialNetworks" is required' })
+	@IsEnum(SocialNetworksEnum, {
+		message:
+			'The "socialNetworks" field must be a valid SocialNetworksEnum value',
+	})
 	socialNetworks: SocialNetworksEnum;
 
-	@ApiProperty()
-	@IsNotEmpty()
-	@IsEnum(SpecializationEnum)
+	@ApiProperty({
+		example: 'Hair Transplant',
+		description: 'Clinic specialization',
+	})
+	@IsNotEmpty({ message: 'The "specializations" is required' })
+	@IsEnum(SpecializationEnum, {
+		message:
+			'The "specializations" field must be a valid SpecializationEnum value',
+	})
 	specializations: SpecializationEnum;
 
-	@ApiProperty()
-	@IsNotEmpty()
-	@IsString()
+	@ApiProperty({
+		example: 'Generic name',
+		description: 'Clinic corporate name or group name',
+	})
+	@IsNotEmpty({ message: 'The "corporateName" is required' })
+	@IsString({ message: 'The "corporateName" field must be a string' })
 	corporateName: string;
 
-	@ApiProperty()
-	@IsNotEmpty()
-	@IsString()
+	@ApiProperty({ example: '12345652912348', description: 'Clinic CNPJ' })
+	@IsNotEmpty({ message: 'The "cnpj" is required' })
+	@IsString({ message: 'The "cnpj" field must be a string' })
 	cnpj: string;
 
-	@ApiProperty()
-	@IsNotEmpty()
+	@ApiProperty({
+		example: 5,
+		description: 'Clinic days worked during the 7-day week',
+	})
+	@IsNotEmpty({ message: 'The "openingDays" is required' })
 	@IsNumber()
 	openingDays: number;
 
-	@ApiProperty()
-	@IsNotEmpty()
-	@IsString()
+	@ApiProperty({
+		example: 'John Doe',
+		description: 'Name of the technician responsible for the clinic',
+	})
+	@IsNotEmpty({ message: 'The "technicalManager" is required' })
+	@IsString({ message: 'The "technicalManager" field must be a string' })
 	technicalManager: string;
 
-	@ApiProperty()
-	@IsNotEmpty()
-	@IsBoolean()
+	@ApiProperty({
+		example: true || false,
+		description: 'Flag that identifies whether the clinic is active',
+	})
+	@IsNotEmpty({ message: 'The "active" is required' })
+	@IsBoolean({ message: 'The "active" field must be a boolean' })
 	active: boolean;
 }
