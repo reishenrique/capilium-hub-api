d172930ecb750d19a895a452a55c265fe65a641a
Henrique Reis <contatohenriquereis@gmail.com>
2025-02-06 19:20:59 -0300

    refactor: updating the query to fetch only clinics with the 'true' in active flag

--- apps/capilium-hub-api/src/clinic/repository/clinic.repository.ts
+++ apps/capilium-hub-api/src/clinic/repository/clinic.repository.ts
@@ -6,28 +6,28 @@ import { Model } from 'mongoose';
 
 @Injectable()
 export class ClinicRepository {
-		constructor(
-			@InjectModel(Clinic.name)
-			private readonly clinicModel: Model<ClinicDocument>,
-		) {}
+	constructor(
+		@InjectModel(Clinic.name)
+		private readonly clinicModel: Model<ClinicDocument>,
+	) {}
 
-		async createClinic(clinic: Clinic): Promise<Clinic> {
-			const newClinic = (await this.clinicModel.create(clinic)).save();
-			return newClinic;
-		}
-
-		async findClinicById(id: string): Promise<Clinic> {
-			const clinic = await this.clinicModel.findById(id).exec();
-			return clinic;
-		}
-
-		async findClinicByCnpj(cnpj: string): Promise<Clinic> {
-			const clinic = await this.clinicModel.findOne({ cnpj }).exec();
-			return clinic;
-		}
-
-		async findAllClinics(): Promise<Clinic[]> {
-			const clinics = await this.clinicModel.find();
-			return clinics;
-		}
+	async createClinic(clinic: Clinic): Promise<Clinic> {
+		const newClinic = (await this.clinicModel.create(clinic)).save();
+		return newClinic;
 	}
+
+	async findClinicById(id: string): Promise<Clinic> {
+		const clinic = await this.clinicModel.findById(id).exec();
+		return clinic;
+	}
+
+	async findClinicByCnpj(cnpj: string): Promise<Clinic> {
+		const clinic = await this.clinicModel.findOne({ cnpj }).exec();
+		return clinic;
+	}
+
+	async findAllActivatedClinics(): Promise<Clinic[]> {
+		const clinics = await this.clinicModel.find({ active: true });
+		return clinics;
+	}
+}
