c343026e15c4e110c2af9cfc9ab282cfe81e0f9a
Henrique Reis <contatohenriquereis@gmail.com>
2025-02-11 18:03:32 -0300

    chore: adjusting the tsconfig alias of the shared app to be recognized in jest config

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,11 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['**/__test__/**/*.spec.ts'],
-  moduleNameMapper: {
-    '^src/(.*)$': '<rootDir>/src/$1',
-  },
+	preset: 'ts-jest',
+	testEnvironment: 'node',
+	testMatch: ['**/__test__/**/*.spec.ts'],
+	moduleNameMapper: {
+		'^src/(.*)$': '<rootDir>/src/$1',
+		'^@app/shared$': '<rootDir>/libs/shared/src',
+		'^@app/shared/(.*)$': '<rootDir>/libs/shared/src/$1',
+	},
 };
