6723782abd910141e857d2a52c06a8eb893f6538
Henrique Reis <contatohenriquereis@gmail.com>
2025-02-20 18:22:10 -0300

    wip: opportunity entity

---
+++ apps/capilium-hub-api/src/opportunity/entity/opportunity.entity.ts
@@ -0,0 +1,38 @@
+import { Schema, Prop } from '@nestjs/mongoose';
+import { ApiProperty } from '@nestjs/swagger';
+import { StatusEnum } from '../../common/enums/status.enum';
+import { Types } from 'mongoose';
+
+@Schema({ timestamps: true })
+export class Opportunity {
+	@ApiProperty()
+	@Prop()
+	title: string;
+
+	@ApiProperty()
+	@Prop()
+	description: string;
+
+	@ApiProperty()
+	@Prop()
+	location: string;
+
+	@ApiProperty()
+	@Prop()
+	salary: number;
+
+	@ApiProperty()
+	@Prop({ type: String, enum: StatusEnum, default: StatusEnum.Open })
+	status: StatusEnum;
+
+	@ApiProperty()
+	@Prop({ type: Types.ObjectId, ref: 'Clinic' })
+	clinicName: string;
+
+	@ApiProperty()
+	@Prop({
+		type: [{ type: Types.ObjectId, ref: 'User', required: true }],
+		default: [],
+	})
+	applicants: Types.ObjectId[];
+}
