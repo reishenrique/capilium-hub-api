eb97abbcfb5f9a8d54e98fec76cb867bd4f6d5f3
Henrique Reis <contatohenriquereis@gmail.com>
2025-02-21 20:00:21 -0300

    wip: implementing query

---
+++ apps/capilium-hub-api/src/opportunity/repositories/opportunity.repository.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Opportunity } from '../entity/opportunity.entity';
+import { Model } from 'mongoose';
+import { OpportunityDocument } from '../schemas/opportunity.schema';
+
+@Injectable()
+export class OpportunityRepository {
+	constructor(
+		@InjectModel(Opportunity.name)
+		private readonly opportunityModel: Model<OpportunityDocument>,
+	) {}
+
+	async findOpportunityById(id: string): Promise<Opportunity> {
+		const opportunity = await this.opportunityModel.findById(id).exec();
+		return opportunity;
+	}
+}
