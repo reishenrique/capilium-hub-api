1889291e68da8df54740c211e4044ce141471d6d
Henrique Reis <contatohenriquereis@gmail.com>
2025-02-22 15:34:09 -0300

    feat: implementing create opportunity service method

--- apps/capilium-hub-api/src/opportunity/opportunity.service.ts
+++ apps/capilium-hub-api/src/opportunity/opportunity.service.ts
@@ -1,12 +1,15 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { OpportunityResponseDto } from './dto/opportunityResponseDto';
 import { OpportunityRepository } from './repositories/opportunity.repository';
+import { OpportunityCreateDto } from './dto/opportunityCreateDto';
 
 @Injectable()
 export class OpportunityService {
 	constructor(private readonly opportunityRepository: OpportunityRepository) {}
 
-	async findOpportunityById(id: string): Promise<OpportunityResponseDto> {
+	async findOpportunityById(
+		id: string,
+	): Promise<Partial<OpportunityResponseDto>> {
 		const opportunity =
 			await this.opportunityRepository.findOpportunityById(id);
 
@@ -15,5 +18,13 @@ export class OpportunityService {
 		}
 
 		return opportunity;
+	}
+
+	async create(
+		opportunity: OpportunityCreateDto,
+	): Promise<Partial<OpportunityResponseDto>> {
+		const newOpportunity = await this.opportunityRepository.create(opportunity);
+
+		return newOpportunity;
 	}
 }
