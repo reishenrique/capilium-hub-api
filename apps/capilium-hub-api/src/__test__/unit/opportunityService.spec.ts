adcc146843f83fd7c2bb35c846043c04f2b27c7b
Henrique Reis <contatohenriquereis@gmail.com>
2025-02-28 10:07:23 -0300

    feat: implementation that creates an opportunity with the applicants array

--- apps/capilium-hub-api/src/__test__/unit/opportunityService.spec.ts
+++ apps/capilium-hub-api/src/__test__/unit/opportunityService.spec.ts
@@ -57,13 +57,50 @@ describe('Opportunity Service', () => {
 					location: expect.any(String),
 					salary: expect.any(Number),
 					status: expect.any(String),
-					clinicName: expect.any(String)
-				})
-			)
+					clinicName: expect.any(String),
+				}),
+			);
 
-			expect(spyCreateOpportunityRepo).toHaveBeenCalledTimes(1)
+			expect(spyCreateOpportunityRepo).toHaveBeenCalledTimes(1);
 			expect(spyOpportunityService).toHaveBeenCalledTimes(1);
+		});
 
+		it('Should create opportunity and candidate array addition', async () => {
+			const opportunityPayload = {
+				title: 'Teste',
+				description: 'Teste',
+				location: 'Teste',
+				salary: 100,
+				status: StatusEnum.Open,
+				clinicName: 'ClinicName',
+				applicants: [],
+			};
+
+			const spyCreateOpportunityRepo = jest
+				.spyOn(opportunityRepository, 'create')
+				.mockResolvedValue(opportunityPayload);
+
+			const spyOpportunityService = jest.spyOn(opportunityService, 'create');
+
+			const opportunity = await opportunityService.create(opportunityPayload);
+
+			expect(opportunity.title).toBe(opportunityPayload.title);
+			expect(opportunity.status).toBe(opportunityPayload.status);
+
+			expect(opportunity).toEqual(
+				expect.objectContaining({
+					title: expect.any(String),
+					description: expect.any(String),
+					location: expect.any(String),
+					salary: expect.any(Number),
+					status: expect.any(String),
+					clinicName: expect.any(String),
+					applicants: expect.any(Array),
+				}),
+			);
+
+			expect(spyCreateOpportunityRepo).toHaveBeenCalledTimes(1);
+			expect(spyOpportunityService).toHaveBeenCalledTimes(1);
 		});
 	});
 	describe('Error Cases', () => {});
