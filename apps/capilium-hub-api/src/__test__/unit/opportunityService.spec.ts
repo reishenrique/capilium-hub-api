4224d92d194cb082af74069819839f328f228e7b
Henrique Reis <contatohenriquereis@gmail.com>
2025-02-28 10:00:51 -0300

    feat: opportunity creation unit test

--- apps/capilium-hub-api/src/__test__/unit/opportunityService.spec.ts
+++ apps/capilium-hub-api/src/__test__/unit/opportunityService.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { OpportunityService } from '../../opportunity/opportunity.service';
 import { OpportunityRepository } from '../../opportunity/repositories/opportunity.repository';
 import { createMock } from '@golevelup/ts-jest';
+import { StatusEnum } from '../../common/enums/status.enum';
 
 describe('Opportunity Service', () => {
 	let opportunityService: OpportunityService;
@@ -27,6 +28,43 @@ describe('Opportunity Service', () => {
 		jest.clearAllMocks();
 	});
 
-	describe('Success Cases', () => {});
+	describe('Success Cases', () => {
+		it('Should create a new opporunity', async () => {
+			const opportunityPayload = {
+				title: 'Teste',
+				description: 'Teste',
+				location: 'Teste',
+				salary: 100,
+				status: StatusEnum.Open,
+				clinicName: 'ClinicName',
+			};
+
+			const spyCreateOpportunityRepo = jest
+				.spyOn(opportunityRepository, 'create')
+				.mockResolvedValue(opportunityPayload);
+
+			const spyOpportunityService = jest.spyOn(opportunityService, 'create');
+
+			const opportunity = await opportunityService.create(opportunityPayload);
+
+			expect(opportunity.title).toBe(opportunityPayload.title);
+			expect(opportunity.status).toBe(opportunityPayload.status);
+
+			expect(opportunity).toEqual(
+				expect.objectContaining({
+					title: expect.any(String),
+					description: expect.any(String),
+					location: expect.any(String),
+					salary: expect.any(Number),
+					status: expect.any(String),
+					clinicName: expect.any(String)
+				})
+			)
+
+			expect(spyCreateOpportunityRepo).toHaveBeenCalledTimes(1)
+			expect(spyOpportunityService).toHaveBeenCalledTimes(1);
+
+		});
+	});
 	describe('Error Cases', () => {});
 });
