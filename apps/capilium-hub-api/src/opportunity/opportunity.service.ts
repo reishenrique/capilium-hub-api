1f231896c795ea4222bcae14a577d0afca9f7036
Henrique Reis <contatohenriquereis@gmail.com>
2025-03-01 14:16:53 -0300

    wip: thinking how can I check if user already apply on opportunity

--- apps/capilium-hub-api/src/opportunity/opportunity.service.ts
+++ apps/capilium-hub-api/src/opportunity/opportunity.service.ts
@@ -1,11 +1,16 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { OpportunityResponseDto } from './dto/opportunityResponseDto';
 import { OpportunityRepository } from './repositories/opportunity.repository';
 import { OpportunityCreateDto } from './dto/opportunityCreateDto';
+import { UserRepository } from '../users/repository/user.repository';
+import { StatusEnum } from '../common/enums/status.enum';
 
 @Injectable()
 export class OpportunityService {
-	constructor(private readonly opportunityRepository: OpportunityRepository) {}
+	constructor(
+		private readonly opportunityRepository: OpportunityRepository,
+		private readonly userRepository: UserRepository,
+	) {}
 
 	async findOpportunityById(
 		id: string,
@@ -26,5 +31,23 @@ export class OpportunityService {
 		const newOpportunity = await this.opportunityRepository.create(opportunity);
 
 		return newOpportunity;
+	}
+
+	// WIP > Thinking how can I check if user already apply on opportunity
+	async applyAtOpportunity(userId: string, opportunityId: string) {
+		const user = await this.userRepository.findUserById(userId);
+
+		if (!user)
+			throw new NotFoundException('User not found to opportunity apply');
+
+		const opportunity =
+			await this.opportunityRepository.findOpportunityById(opportunityId);
+
+		if (!opportunity)
+			throw new NotFoundException('Opportunity not found to user apply');
+
+		if (opportunity.status !== StatusEnum.Open) {
+			throw new BadRequestException('Opportunity is not open for applications')
+		}
 	}
 }
