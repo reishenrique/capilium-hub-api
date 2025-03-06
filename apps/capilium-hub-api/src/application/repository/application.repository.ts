e542fec4bcbbf5449a3635c8568c06917937ffaf
Henrique Reis <contatohenriquereis@gmail.com>
2025-03-06 17:55:03 -0300

    feat: implementing get querys by user, application and id

--- apps/capilium-hub-api/src/application/repository/application.repository.ts
+++ apps/capilium-hub-api/src/application/repository/application.repository.ts
@@ -17,4 +17,18 @@ export class ApplicationRepository {
 		opportunityId: string,
 	): Promise<Application> {}
 
+	async listApplicationByUser(id: string): Promise<Application> {
+		const application = await this.applicationModel.findOne({ _id: id }).exec();
+		return application;
+	}
+
+	async listApplicationByOpportunity(id: string): Promise<Application> {
+		const application = await this.applicationModel.findOne({ _id: id }).exec();
+		return application;
+	}
+
+	async listApplicationById(id: string) {
+		const application = await this.applicationModel.findOne({ _id: id }).exec();
+		return application;
+	}
 }
