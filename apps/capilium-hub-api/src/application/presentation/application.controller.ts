994ea72a278979228f56d5b65227b773718623bd
Henrique Reis <contatohenriquereis@gmail.com>
2025-03-11 18:19:17 -0300

    feat: create application endpoint controller

---
+++ apps/capilium-hub-api/src/application/presentation/application.controller.ts
@@ -0,0 +1,35 @@
+import {
+	Body,
+	Controller,
+	HttpCode,
+	HttpStatus,
+	Logger,
+	Post,
+} from '@nestjs/common';
+import { ApplicationService } from '../application.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApplicationCreateDto } from '../dto/applicationCreateDto';
+
+@ApiTags('application')
+@Controller('application')
+export class ApplicationController {
+	protected readonly _logger = new Logger(ApplicationController.name);
+	constructor(private readonly applicationService: ApplicationService) {}
+
+	@Post('/createApplication')
+	@HttpCode(HttpStatus.CREATED)
+	@ApiOperation({
+		summary: 'Creates registry of user application for opportunity',
+	})
+	@ApiResponse({ status: 201 })
+	@ApiResponse({ status: 404, description: 'User not found' })
+	@ApiResponse({
+		status: 409,
+		description: 'User has already applied for this opportunity',
+	})
+	public async createApplication(
+		@Body() applicationPayload: ApplicationCreateDto,
+	) {
+		return await this.applicationService.createApplication(applicationPayload);
+	}
+}
