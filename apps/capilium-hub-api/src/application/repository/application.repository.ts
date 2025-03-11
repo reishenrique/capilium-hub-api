20032a495c3727a7b18e20e52a4f7d763c5fc0a3
Henrique Reis <contatohenriquereis@gmail.com>
2025-03-11 18:20:21 -0300

    feat: create application query

--- apps/capilium-hub-api/src/application/repository/application.repository.ts
+++ apps/capilium-hub-api/src/application/repository/application.repository.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Application } from '../entity/application.entity';
-import { Model } from 'mongoose';
+import { Model, Types } from 'mongoose';
 import { ApplicationDocument } from '../schemas/applications.schema';
 
 @Injectable()
@@ -11,11 +11,19 @@ export class ApplicationRepository {
 		private readonly applicationModel: Model<ApplicationDocument>,
 	) {}
 
-	// WIP
 	async createApplication(
-		userId: string,
 		opportunityId: string,
-	): Promise<Application> {}
+		userId: string,
+	): Promise<Application> {
+		const createApplication = (
+			await this.applicationModel.create({
+				opportunityId: new Types.ObjectId(opportunityId),
+				userId: new Types.ObjectId(userId),
+			})
+		).save();
+
+		return createApplication;
+	}
 
 	async listApplicationByUser(id: string): Promise<Application> {
 		const application = await this.applicationModel.findOne({ _id: id }).exec();
