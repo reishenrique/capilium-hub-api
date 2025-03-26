2ed5c8df3b21ad9f9ac116ca9fc4394ba22d1c9c
Henrique Reis <contatohenriquereis@gmail.com>
2025-03-26 18:07:08 -0300

    refactor: add try catch block and adjusting logger messages

--- apps/queue/src/mailer/mailer.processor.ts
+++ apps/queue/src/mailer/mailer.processor.ts
@@ -3,16 +3,28 @@ import { Process, Processor } from '@nestjs/bull';
 import { Logger } from '@nestjs/common';
 import { send } from 'apps/queue/common/utils/mailerUtils';
 import { Job } from 'bull';
+import { IEmailJobData } from 'apps/queue/common/interfaces/IEmailJobData';
 
 @Processor(EMAIL_QUEUE)
 export class EmailProcessor {
 	private _logger = new Logger(EmailProcessor.name);
 
 	@Process('send-email')
-	async handleEmailJob(job: Job) {
-		const { to, subject, body } = job.data;
+	async handleEmailJob(job: Job<IEmailJobData>) {
+		const { to, subject, body, metadata } = job.data;
 
-		await send(to, subject, body);
-		this._logger.debug(job.data);
+		try {
+			await send(to, subject, body);
+			this._logger.log(
+				`Email sent successfully to ${to} | Data: ${JSON.stringify(job.data, null, 2)}`,
+			);
+		} catch (error) {
+			this._logger.error(
+				`Failed to send email (Type: ${metadata?.emailType}) to ${to} | Error: ${error.message}
+				`,
+				error.stack,
+			);
+			throw error;
+		}
 	}
 }
