9cafc9024f4c6242aeeb868f8fb177e17e37a2e9
Henrique Reis <contatohenriquereis@gmail.com>
2025-03-28 14:36:36 -0300

    fix: removing application method that is not being used

--- apps/capilium-hub-api/src/opportunity/opportunity.service.ts
+++ apps/capilium-hub-api/src/opportunity/opportunity.service.ts
@@ -1,22 +1,12 @@
-import {
-	BadRequestException,
-	Injectable,
-	Logger,
-	NotFoundException,
-} from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { OpportunityResponseDto } from './dto/opportunityResponseDto';
 import { OpportunityRepository } from './repositories/opportunity.repository';
 import { OpportunityCreateDto } from './dto/opportunityCreateDto';
-import { UserRepository } from '../users/repository/user.repository';
-import { StatusEnum } from '../common/enums/status.enum';
 
 @Injectable()
 export class OpportunityService {
 	protected readonly _logger = new Logger('OpportunityService');
-	constructor(
-		private readonly opportunityRepository: OpportunityRepository,
-		private readonly userRepository: UserRepository,
-	) {}
+	constructor(private readonly opportunityRepository: OpportunityRepository) {}
 
 	async findOpportunityById(
 		id: string,
@@ -48,29 +38,6 @@ export class OpportunityService {
 		const newOpportunity = await this.opportunityRepository.create(opportunity);
 
 		return newOpportunity;
-	}
-
-	// WIP > Thinking how can I check if user already apply on opportunity
-	async applyAtOpportunity(userId: string, opportunityId: string) {
-		const user = await this.userRepository.findUserById(userId);
-
-		if (!user) {
-			this._logger.error(`User ID: ${userId} not found to apply`);
-			throw new NotFoundException('User not found to opportunity apply');
-		}
-
-		const opportunity =
-			await this.opportunityRepository.findOpportunityById(opportunityId);
-
-		if (!opportunity) {
-			this._logger.error(`Opportunity ID: ${opportunityId} not found`);
-			throw new NotFoundException('Opportunity not found to user apply');
-		}
-
-		if (opportunity.status !== StatusEnum.Open) {
-			this._logger.error('Opportunity status is different from active');
-			throw new BadRequestException('Opportunity is not open for applications');
-		}
 	}
 
 	async deleteOpportunityById(id: string): Promise<void> {
