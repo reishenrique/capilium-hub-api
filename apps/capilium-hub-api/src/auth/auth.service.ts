f9646663897c244c3a2e7115ccff1d1f53c91103
Henrique Reis <contatohenriquereis@gmail.com>
2025-07-02 18:15:09 -0300

    feat: implementing logging in auth service methods

--- apps/capilium-hub-api/src/auth/auth.service.ts
+++ apps/capilium-hub-api/src/auth/auth.service.ts
@@ -15,25 +15,60 @@ import {
 	refreshAccessToken,
 } from '../common/helpers/jwt.helper';
 import { UserRepository } from '../users/repository/user.repository';
+import { LogEventEnum } from '../logger/enum/log-event.enum';
+import EventEmitter2 from 'eventemitter2';
+import { LogLevelEnum } from '../logger/enum/log-level.enum';
 
 @Injectable()
 export class AuthService {
 	protected readonly _logger = new Logger('AuthService');
+	private eventEmitter: EventEmitter2;
 	constructor(private readonly userRepository: UserRepository) {}
 
 	async login(login: LoginDto): Promise<LoginResponseDto> {
 		const user = await this.userRepository.findUserByEmail(login.email);
 
-		if (!user) throw new UnauthorizedException('Unauthorized');
+		if (!user) {
+			this.eventEmitter.emit(LogEventEnum.InternalLog, {
+				level: LogLevelEnum.Error,
+				message: 'User not found',
+				context: 'AuthService',
+				data: {
+					email: login.email,
+				},
+			});
+
+			throw new UnauthorizedException('Unauthorized');
+		}
 
 		const isValidPassword = await bcrypt.compareSync(
 			login.password,
 			user.password,
 		);
 
-		if (!isValidPassword) throw new UnauthorizedException('Invalid password');
+		if (!isValidPassword) {
+			this.eventEmitter.emit(LogEventEnum.InternalLog, {
+				level: LogLevelEnum.Error,
+				message: 'Invalid password',
+				context: 'AuthService',
+				data: {
+					email: login.email,
+				},
+			});
+
+			throw new UnauthorizedException('Invalid password');
+		}
 
 		const token = generateAccessToken(user);
+
+		this.eventEmitter.emit(LogEventEnum.InternalLog, {
+			level: LogLevelEnum.Success,
+			message: 'User logged',
+			context: 'AuthService',
+			data: {
+				email: login.email,
+			},
+		});
 
 		return {
 			message: 'Login successful!',
@@ -45,6 +80,16 @@ export class AuthService {
 		refreshAuthCredentials: RefreshAuthCredentialsDto,
 	): Promise<RefreshAuthResponseDto> {
 		if (!refreshAuthCredentials.email || !refreshAuthCredentials.refreshToken) {
+			this.eventEmitter.emit(LogEventEnum.InternalLog, {
+				level: LogLevelEnum.Error,
+				message: 'User email and refresh token is required',
+				context: 'AuthService',
+				data: {
+					email: refreshAuthCredentials.email,
+					refreshToken: refreshAuthCredentials.refreshToken,
+				},
+			});
+
 			throw new BadRequestException(
 				'User email and refresh token is required to proceed with refresh',
 			);
@@ -54,12 +99,32 @@ export class AuthService {
 			refreshAuthCredentials.email,
 		);
 
-		if (!user) throw new NotFoundException('User not found');
+		if (!user) {
+			this.eventEmitter.emit(LogEventEnum.InternalLog, {
+				level: LogLevelEnum.Error,
+				message: 'User not found',
+				context: 'AuthService',
+				data: {
+					email: refreshAuthCredentials.email,
+				},
+			});
+
+			throw new NotFoundException('User not found');
+		}
 
 		const token = refreshAccessToken(
 			refreshAuthCredentials.refreshToken,
 			refreshAuthCredentials.email,
 		);
+
+		this.eventEmitter.emit(LogEventEnum.InternalLog, {
+			level: LogLevelEnum.Success,
+			message: 'Access token updated',
+			context: 'AuthService',
+			data: {
+				email: refreshAuthCredentials.email,
+			},
+		});
 
 		return {
 			message: 'Access token updated successfully',
