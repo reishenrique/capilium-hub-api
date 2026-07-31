7adbb6e503a5f48d5b681b6ece36519c182c43a0
Henrique Reis <contatohenriquereis@gmail.com>
2026-07-31 11:41:00 -0300

    rft: adjusting the unique name for resume uploads using a random uuid and extname

--- apps/capilium-hub-api/src/upload/helpers/upload.helper.ts
+++ apps/capilium-hub-api/src/upload/helpers/upload.helper.ts
@@ -1,10 +1,12 @@
 import { BadRequestException } from '@nestjs/common';
 import { diskStorage } from 'multer';
+import { randomUUID } from 'node:crypto';
+import { extname } from 'node:path';
 
 export const multerStorage = diskStorage({
 	destination: './uploads/resumes',
 	filename: (_req, file, cb) => {
-		const uniqueName = `${Date.now()}-${file.originalname}`;
+		const uniqueName = `${randomUUID()}${extname(file.originalname)}`;
 		cb(null, uniqueName);
 	},
 });
