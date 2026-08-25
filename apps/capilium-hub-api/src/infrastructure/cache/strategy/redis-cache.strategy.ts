e6ad669583fd9b4b12fa6c9146a5de49b850c6c2
Henrique Reis <contatohenriquereis@gmail.com>
2026-08-25 10:38:23 -0300

    feat: implementing set cache

---
+++ apps/capilium-hub-api/src/infrastructure/cache/strategy/redis-cache.strategy.ts
@@ -0,0 +1,21 @@
+import { Inject, Injectable } from '@nestjs/common';
+import AbstractCacheStrategy from './abstract-cache.strategy';
+import Redis from 'ioredis';
+
+@Injectable()
+export default class RedisCacheStrategy extends AbstractCacheStrategy {
+	constructor(@Inject('REDIS_CLIENT') private readonly redis: Redis) {
+		super();
+	}
+
+	public async set<T>(key: string, value: T, ttl?: number): Promise<void> {
+		const serializedValue = JSON.stringify(value);
+
+		if (ttl !== undefined) {
+			await this.redis.set(key, serializedValue, 'EX', ttl);
+			return;
+		}
+
+		await this.redis.set(key, serializedValue);
+	}
+}
