ef41d4d224e04dae559413627c7f6537d311fb42
Henrique Reis <contatohenriquereis@gmail.com>
2026-08-25 10:38:10 -0300

    feat: implementing a redis provider that makes a instance available for cache usage

---
+++ apps/capilium-hub-api/src/common/providers/redis.provider.ts
@@ -0,0 +1,12 @@
+import Redis from 'ioredis';
+
+export const redisProvider = {
+	provide: 'REDIS_CLIENT',
+	useFactory: () => {
+		return new Redis({
+			host: process.env.REDIS_HOST,
+			port: Number(process.env.REDIS_PORT),
+			password: process.env.REDIS_PASSWORD,
+		});
+	},
+};
