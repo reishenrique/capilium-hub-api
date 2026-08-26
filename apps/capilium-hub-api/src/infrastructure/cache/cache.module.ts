745f9711d61caed4ec502270211adcd737d1753f
Henrique Reis <contatohenriquereis@gmail.com>
2026-08-26 17:27:36 -0300

    feat: adding redis cache strategy into cache module

--- apps/capilium-hub-api/src/infrastructure/cache/cache.module.ts
+++ apps/capilium-hub-api/src/infrastructure/cache/cache.module.ts
@@ -2,11 +2,12 @@ import { Global, Module } from '@nestjs/common';
 import { CacheService } from './cache.service';
 import { CacheController } from './presentation/cache.controller';
 import NodeCacheStrategy from './strategy/node-cache.strategy';
+import RedisCacheStrategy from './strategy/redis-cache.strategy';
 
 @Global()
 @Module({
 	controllers: [CacheController],
-	providers: [CacheService, NodeCacheStrategy],
+	providers: [CacheService, NodeCacheStrategy, RedisCacheStrategy],
 	exports: [CacheService],
 })
 export class CacheModule {}
