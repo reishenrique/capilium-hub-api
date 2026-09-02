b80c5067296c7a004182ffb400042bf3fc6f51a7
Henrique Reis <contatohenriquereis@gmail.com>
2026-09-02 17:12:59 -0300

    feat: cache keys enum

---
+++ apps/capilium-hub-api/src/common/enums/cache-keys.enum.ts
@@ -0,0 +1,6 @@
+export enum CacheKeyEnum {
+	CLINIC_ACTIVATED = 'clinic:activated',
+	OPPORTUNITY = 'opportunity',
+	OPPORTUNITIES_OPENED = 'opportunities:opened',
+	OPPORTUNITY_ACTIVATED = 'opportunity:activated',
+}
