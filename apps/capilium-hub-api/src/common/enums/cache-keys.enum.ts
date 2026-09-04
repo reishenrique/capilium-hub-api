63a0c8ea4a662f8b23c7a5437bd362abf944d854
Henrique Reis <contatohenriquereis@gmail.com>
2026-09-04 17:22:55 -0300

    feat: adding clinic cache key

--- apps/capilium-hub-api/src/common/enums/cache-keys.enum.ts
+++ apps/capilium-hub-api/src/common/enums/cache-keys.enum.ts
@@ -1,5 +1,6 @@
 export enum CacheKeyEnum {
 	CLINIC_ACTIVATED = 'clinic:activated',
+	CLINIC = 'clinic',
 	OPPORTUNITY = 'opportunity',
 	OPPORTUNITIES_OPENED = 'opportunities:opened',
 	OPPORTUNITY_ACTIVATED = 'opportunity:activated',
